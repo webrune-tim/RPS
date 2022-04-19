d2e967a82d7437c2e39c597e408ba12f4970fffb
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-04-19 14:34:52 -0700

    done with basic functionality

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,23 @@
   import '../styles.css';
 </script>
 
+<header>
+  <h1>✊🏼, 🖐🏼, ✌🏼</h1>
+  <h2>Rock, Paper, Scissors!</h2>
+</header>
 <main>
   <slot />
 </main>
+<footer>
+  <p>Created with ❤️ by <a href="https://timsmith.tech">Tim Smith</a></p>
+</footer>
+
+<style>
+  header {
+  }
+  footer {
+    padding: 3rem;
+    text-align: center;
+    color: var(--text-color);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,25 @@
 <script>
   const choices = ['✊🏼', '🖐🏼', '✌🏼'];
-  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-  let choiceIndex = 0;
+  let yourIndex = 0;
   let theirIndex = 0;
-	let key;
 	let keyCode;
-  let playing = false;
-  let randomNumber = 0;
+  let playing = '';
+  let currentIndex = 0;
+  let winner = '';
 
   function left() {
-    if (choiceIndex === 0) {
-      choiceIndex = choices.length - 1;
+    if (yourIndex === 0) {
+      yourIndex = choices.length - 1;
     } else {
-      choiceIndex--;
+      yourIndex--;
     }
   }
 
   function right() {
-    if (choiceIndex === choices.length - 1) {
-      choiceIndex = 0;
+    if (yourIndex === choices.length - 1) {
+      yourIndex = 0;
     } else {
-      choiceIndex++;
+      yourIndex++;
     }
   }
 
@@ -33,67 +32,127 @@
   }
 
   function handleKeydown(event) {
-		key = event.key;
 		keyCode = event.keyCode;
-    if (keyCode === 37) {
-      left();
-    } else if (keyCode === 39) {
-      right();
+    switch (keyCode) {
+      case 37:
+        left();
+        break;
+      case 39:
+        right();
+        break;
+      case 13:
+        play();
+        break;
+      default:
+        break;
     }
 	}
 
-  function play() {
-    randomNumber = Math.floor(Math.random() * 20);
-    console.log('random', randomNumber);
-    playing = true;
-    console.log('playing', playing);
-    while (randomNumber > 0) {
-      theirLeft();
-      randomNumber--;
+  function whoWon() {
+    if (yourIndex === currentIndex) {
+      return 'It\'s a tie!';
+    } else if (yourIndex === 0 && currentIndex === 2) {
+      return 'You won :)';
+    } else if (yourIndex === 1 && currentIndex === 0) {
+      return 'You won :)';
+    } else if (yourIndex === 2 && currentIndex === 1) {
+      return 'You won :)';
+    } else {
+      return 'They won :(';
     }
-    playing = false;
+  }
+
+  function play() {
+    let randomNumber = Math.floor(Math.random() * 20);
+    playing = 'playing';
+    setInterval(() => {
+      if (randomNumber > 0) {
+        theirLeft();
+        randomNumber--;
+        currentIndex = theirIndex;
+      } else {
+        playing = 'done'
+        winner = whoWon()
+      }
+    }, 250)
+  }
+
+  function reset() {
+    playing = '';
+    yourIndex = 0;
+    theirIndex = 0;
+    currentIndex = 0;
+    winner = '';
   }
 </script>
 
 <svelte:window on:keydown={handleKeydown}/>
 
-<h1>✊🏼, 🖐🏼, ✌🏼</h1>
-<h2>Rock, Paper, Scissors!</h2>
-
-<section class="my-choice">
-  <div class="choice">
-    <button on:click={left}>◀</button>
-    <p>{choices[choiceIndex]}</p>
-    <button on:click={right}>▶</button>
-  </div>
-  <p class="choice-p">What is your choice?</p>
-</section>
-<button class="play-button" on:click={play}>▶</button>
-<section class="their-choice">
-  {#if playing}
-    <p>{choices[theirIndex]}</p>
-  {:else}
-    <p>❔</p>
+<div class='wrapper'>
+  {#if playing === 'done'}
+    <section class="winner">{winner}</section>
   {/if}
-</section>
+  <section class='my-choice'>
+    <div class='choice'>
+      <button on:click={left}>◀</button>
+      <p>{choices[yourIndex]}</p>
+      <button on:click={right}>▶</button>
+    </div>
+    <p class='choice-p'>What is your choice?</p>
+  </section>
+  <section class='their-choice'>
+    {#if playing === 'playing'}
+      <p>{choices[theirIndex]}</p>
+    {:else if playing === 'done'}
+      <button class='reset-button' on:click={reset}>↺</button>
+      <p>{choices[currentIndex]}</p>
+    {:else}
+      <button class='play-button' on:click={play}>▶</button>
+      <p>❔</p>
+    {/if}
+  </section>
+</div>
 
 
-<style >
+<style>
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .middle,
+  .my-choice,
+  .their-choice{
+    margin: 2rem auto;
+    flex: 1 1 550px;
+    gap: 2rem;
+  }
+
+  .winner,
   .my-choice,
   .their-choice {
-    width: 45vw;
-    margin: 2rem auto;
+    min-height: 245px;
     border: 6px var(--text-color) solid;
     border-radius: 4rem;
     font-size: 5rem;
     padding: 2rem;
+    margin: 1rem;
+  }
+
+  .winner {
+    width: 100%;
+    display: grid;
+    place-items: center;
   }
 
   .their-choice p {
     text-align: center;
   }
 
-  .choice {
+  .choice,
+  .their-choice {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -104,8 +163,7 @@
     text-align: center;
   }
 
-  .my-choice button,
-  .play-button {
+  .my-choice button {
     font-size: 2rem;
     width: 4rem;
     height: 3rem;
@@ -119,8 +177,20 @@
   .play-button {
     display: block;
     background: transparent;
-    color: var(--text-color);
+    color: var(--highlight-color);
     font-size: 5rem;
-    margin: -1rem auto 6rem auto;
+    -webkit-text-stroke: 4px var(--text-color);
+    margin-bottom: 0.5rem;
+    cursor: pointer;
+  }
+
+  .reset-button {
+    display: block;
+    background: transparent;
+    color: var(--highlight-color);
+    font-size: 5rem;
+    -webkit-text-stroke: 4px var(--text-color);
+    margin-bottom: 0.5rem;
+    cursor: pointer;
   }
 </style>
